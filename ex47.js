350fb8f183e21c9fb533e9a21d47b068f18e6e96
Jackson Wanjala <61497543+JacksonWanjala@users.noreply.github.com>
2026-02-20 19:41:41 +0300

    Create ex47.js

---
+++ ex47.js
@@ -0,0 +1,8 @@
+// DO NOT ALTER THE FOLLOWING CODE:
+const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+
+// Write your code 
+const firstNames = fullNames.map(person => person.first);
+console.log(firstNames);
+
+
