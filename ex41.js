e22960123010f09586be7a0f175a8890296e9ed6
Jackson Wanjala <61497543+JacksonWanjala@users.noreply.github.com>
2026-02-20 19:29:22 +0300

    Create ex41.js

---
+++ ex41.js
@@ -0,0 +1,7 @@
+// DEFINE YOUR FUNCTION BELOW:
+function capitalize(word) {
+    const firstLetterCaps = word[0].toUpperCase();
+    const restOfString = word.slice(1);
+    return firstLetterCaps + restOfString;
+    
+}
